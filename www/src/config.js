bd79be7ac564892bcc033749bd27ee58277e1292
Upendra Sahu <upendra.sahu@maplelabs.com>
2021-06-02 21:10:40 +0530

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "dusl61vs6k35avqjpgl7qldig",     // CognitoClientID
+  "api_base_url": "https://0enxlncoq4.execute-api.us-west-2.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-upen-lambda-refarch-webapp.auth.us-west-2.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d13a6jy4ymr2vi.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
